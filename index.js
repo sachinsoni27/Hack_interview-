5bcebeb68fabfce78990715ec460e038199aba2d
sachinsoni27 <sachinsoniofficial2003@gmail.com>
2026-04-06 23:52:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from  "dotenv";
+
+dotenv.config();  
+
+const app = express();
+
+
+const PORT = process.env.PORT || 6000 
+
+app.get("/", (req, res) => {
+  return res.json({message:"server started "});
+})
+
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+})
